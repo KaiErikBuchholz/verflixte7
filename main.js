1797ddccc3a946abfc7791f15b34987a2e9ff043
kaibu <kaibuchholz13@gmail.com>
2022-05-14 22:54:38 +0200

    function

--- main.js
+++ main.js
@@ -3,12 +3,17 @@
 
 
 
-
-
-
 function würfeln(){
 
+    let a = Math.floor(Math.random()*6+1)
+    let b = Math.floor(Math.random()*6+1)
 
+    let c = a + b + c
+    let ars1 = ars1 + 1
+    console.log(c)
+    if(c === 7){
+
+    }
 }
 function beenden(){
 
